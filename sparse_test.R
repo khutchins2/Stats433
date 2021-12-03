e7e57eb0af54959b61428e8fd34c979d2e46f622
Jake Fenley <jakepfenley@gmail.com>
2021-12-03 16:29:08 -0600

    predictions

--- sparse_test.R
+++ sparse_test.R
@@ -8,6 +8,9 @@ library(factoextra)
 library(RSpectra)
 library(spam)
 library(MatrixExtra)
+library(ramify)
+library(data.table)
+library(doBy)
 
 # Create data files using: https://www.kaggle.com/jfenley/spotify-sparse
 
@@ -25,12 +28,12 @@ sum(dt) # Should equal ~65M
 ###################### Train/Valid/Test Splits #################################
 set.seed(101)
 
-sample <- sample.int(n = nrow(dt), size=800000, replace=F)
+sample <- sample.int(n = nrow(dt), size=900000, replace=F)
 
 train <- dt[sample, ]
 test <- dt[-sample, ]
 
-sample2 <- sample.int(n = nrow(test), size=100000, replace=F)
+sample2 <- sample.int(n = nrow(test), size=50000, replace=F)
 valid <- test[sample2, ]
 test <- test[-sample2, ]
 
@@ -55,55 +58,35 @@ get.betas <- function(mat, k){
   return (list('X' = nice_X, 'beta' = nice_beta))
 }
 
-shuffle.items2 <- function(mat, method, n){
-  rowSums = rowSums(mat)
-  major = mat
-  minor = mat
-  for (i in 1:nrow(mat)){
-    if (i %% 10 == 0){
-      cat('At iteration', i, 'of', nrow(mat), '\n')
-    }
-    
-    pos = which(mat[i,] > 0, arr.ind = T)
-    if (method == 'lno'){ # Leave n of the items out
-      ind <- sample.int(n = length(pos), size=length(pos)-n, replace=F)
-    } else if (method == 'split'){ # Split with prob n into each set      ind <- sample.int(n = length(pos), size=length(pos)*n, replace=F)
-    }
-    minor[i, ind] = 1
-    major[i, setdiff(pos, ind)] = 1
-  }
-  return(list('major' = major, 'minor' = minor))
-}
-
-shuffle.items <- function(mat, method, n){
-  rowSums = rowSums(mat)
-  major = mat
+# Creates two data structures:
+# - major: a sparse matrix where each song is removed from each playlist
+# - minor: a data table indicating which song was removed from each playlist
+shuffle.items <- function(mat, n){
+  major = mat 
   
   summ = summary(valid)
   loo = summ %>% group_by(i) %>% sample_n(1) # Leave-one-out from each row
   
-  minor = sparseMatrix(i=loo$i, j=loo$j, x=loo$x, dims=dim(mat)) # Minor matrix is the one song left out from each row.
-  cat('Done creating minor matrix')
+  #minor = sparseMatrix(i=loo$i, j=loo$j, x=loo$x, dims=dim(mat)) # Minor matrix is the one song left out from each row.
+  cat('Done creating minor matrix\n')
+  
+  summ = data.table(summ)
+  setkeyv(summ, c("i", "j")) # Set key for O(log n) searches
+  cat('Done setting key\n')
+  
   for (i in seq_along(loo$i)){ # Remove that one song from ecah row in major matrix.
-    if (i %% 1000 == 0){
-      cat('At iteration', i, 'of 100000\n')
+    if (i %% 5000 == 0){
+      cat('At iteration', i, 'of 50000\n')
     }
     row <- loo[i, ]
-    #summ[row$i, row$j] = 0
-    #major[row$i, row$j] = 0
-    #set(major, i=row$i, j=row$j, value=0)
-    summ[(summ$i == row$i & summ$j == row$j), 3] = 0
-    }
-  return(list('major' = major, 'minor' = minor))
+    summ[.(row$i, row$j), 3] = 0
+  }
+  major = sparseMatrix(i = summ$i, j = summ$j, x=summ$x, dims = dim(mat))
+  return(list('major' = major, 'minor' = loo))
 }
 
 
-
 ##########################      SVD     ################################
-
-# None of these techniques seem to work as they all try to find a covariance matrix
-# which would be 6.6 million songs x 6.6 million songs = 9TB. Need to figure out
-# a more efficient way, such as SVD.
 
 rs = rowSums(train)
 cs = colSums(train)
@@ -111,18 +94,49 @@ L = Diagonal(nrow(train), 1/sqrt(rs+mean(rs)))%*%train%*%Diagonal(ncol(train), 1
 
 
 res = get.betas(L, 10)
-X= res$X
+X = res$X
 betas = res$beta
 
 rm(cs, rs, res)
 gc()
 
+# Create two datasets: one with one song per playlist, one with all but one
 system.time({
-  mats = shuffle.items(valid, 'lno', 1)
+  mats = shuffle.items(valid, 1)
 })
 
+test = mats$major
+true = mats$minor
 
-install.packages("devtools")
-devtools::install_github("RoheLab/vsp")
-library(vsp)
-fa = vsp(dt, rank=30)
+rm(mats)
+gc()
+
+
+# Generate predictions (computer seems to be able to handle up to 
+# 50-100 playlists at a time, more than that crashes)
+preds = test[1:50,] %*% betas %*% t(betas)
+
+
+# Check predictions.
+# TODO: If a prediction can be a song that is already in playlist,
+# need to check and remove it.
+correct = 0
+incorrect = 0
+for (chunk in 1:(nrow(test)/50)){
+  start = proc.time()
+  start_ind = (chunk - 1) * 50 + 1
+  end_ind = start_ind + 49
+  preds = test[start_ind:end_ind,] %*% betas %*% t(betas)
+  for (i in 1:nrow(preds)){
+    row = preds[i,]
+    true_song = true[start_ind + i - 1, 2]
+    guessed = sum(which.maxn(row, 50)  == true_song)
+    if (guessed){
+      correct = correct + 1
+    } else {
+      incorrect = incorrect + 1
+    }
+  }
+  cat("Accuracy:", correct / (correct + incorrect), 'N:', correct+incorrect , "Time:", (proc.time()-start)[3],'s.\n')
+}
+
